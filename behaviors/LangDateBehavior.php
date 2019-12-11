9514af05666df8b471b6f8eebd9e279d50b4fb44
skazkin <russkazkin@gmail.com>
2019-12-11 14:38:22 +0300

    Add events for Article class

--- behaviors/LangDateBehavior.php
+++ behaviors/LangDateBehavior.php
@@ -7,6 +7,7 @@ namespace app\modules\lang\behaviors;
 use DateTime;
 use Yii;
 use yii\base\Behavior;
+use yii\db\ActiveRecord;
 
 class LangDateBehavior extends Behavior
 {
@@ -20,6 +21,11 @@ class LangDateBehavior extends Behavior
 
         $this->format = Yii::$app->language == 'en-US' ? 'mm-dd-yyyy' : 'dd-mm-yyyy';
         $this->phpFormat = substr(Yii::$app->formatter->dateFormat, 4, 5);
+    }
+
+    public function events()
+    {
+        return [ActiveRecord::EVENT_BEFORE_VALIDATE => 'phpToTimestamp'];
     }
 
     public function phpToTimestamp()
