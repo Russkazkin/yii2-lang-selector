7f6e101ec7a2524715c73d0b99e20d70dfe2564c
skazkin <russkazkin@gmail.com>
2019-12-11 14:12:08 +0300

    Add LangDateBehavior class

---
+++ behaviors/LangDateBehavior.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace app\modules\lang\behaviors;
+
+
+use DateTime;
+use Yii;
+use yii\base\Behavior;
+
+class LangDateBehavior extends Behavior
+{
+    public $attributeName;
+    public $format;
+    public $phpFormat;
+
+    public function init()
+    {
+        parent::init();
+
+        $this->format = Yii::$app->language == 'en-US' ? 'mm-dd-yyyy' : 'dd-mm-yyyy';
+        $this->phpFormat = substr(Yii::$app->formatter->dateFormat, 4, 5);
+    }
+
+    public function phpToTimestamp()
+    {
+        $date = DateTime::createFromFormat($this->phpFormat, $this->owner->{$this->attributeName});
+
+        if ($date) {
+            $this->owner->{$this->attributeName} = $date->getTimestamp();
+        }
+    }
+}
